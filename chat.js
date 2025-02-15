ef429a44a1312a2dac6f365f73ff223d4ee905da
the-chicken-man <856030@dpsk12.net>
2025-02-15 10:12:21 -0700

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
 // Initialize Ably Chat
-const ably = new Ably.Realtime('YOUR_ABLY_API_KEY');
+const ably = new Ably.Realtime('mGqA8g.tKm8cg:BXet5NQMtxfr8k8I4Ls0MEzeQxmsQHihc80xDsYd-Ko');
 const channel = ably.channels.get('boredom-hub-chat');
 
 const emailInput = document.getElementById('email');
